c18f3acf56b77308a9c48e58232d06a19b49e4a3
AlexCherniuk <60302722+AlexCherniuk@users.noreply.github.com>
2022-11-02 21:55:50 +0100

    js_define

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,21 @@
 // const isChecked = true;
 // const isOpen = false;
-
 // console.log(isChecked || isOpen);
+const numberOfFilms = prompt("Скільки фільмів ви вже переглянули?", 10);
+let lastFilmQuestion = prompt("Один з останніх переглянутих фільмів?", "");
+let countLastFilm = prompt("Ваша оцінка останньому фільму", "");
+const personalMovieDB = {
+    count: numberOfFilms, 
+    movies: {
+        
+    }, 
+    actors: {}, 
+    genres: [],
+    privat: false,
+};
+
+personalMovieDB.movies[lastFilmQuestion] = countLastFilm;
+
+
+console.log(numberOfFilms);
+console.log(personalMovieDB.movies);
