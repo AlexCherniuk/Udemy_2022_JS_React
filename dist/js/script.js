dd56e2fc221cf0f919b98feb0c01e72406248758
AlexCherniuk <60302722+AlexCherniuk@users.noreply.github.com>
2022-11-06 18:16:19 +0100

    use_function_29

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,70 @@
-// const isChecked = true;
-// const isOpen = false;
-// console.log(isChecked || isOpen);
-const numberOfFilms = prompt("Скільки фільмів ви вже переглянули?", 10);
-let lastFilmQuestion = prompt("Один з останніх переглянутих фільмів?", "");
-let countLastFilm = prompt("Ваша оцінка останньому фільму", "");
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == false || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
 const personalMovieDB = {
-    count: numberOfFilms, 
-    movies: {
-        
-    }, 
-    actors: {}, 
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
     genres: [],
-    privat: false,
+    privat: false
 };
 
-personalMovieDB.movies[lastFilmQuestion] = countLastFilm;
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
 
+function writeYourGenres () {
 
-console.log(numberOfFilms);
-console.log(personalMovieDB.movies);
+    for(let i = 1; i < 4; i++) {
+        const favoriteGenres = prompt(`Ваш любимый жанр под номером ${i}`, '');
+        personalMovieDB.genres[i - 1] = favoriteGenres;
+    }
+    console.log(personalMovieDB);
+}
+writeYourGenres();
+
+// function rememberNyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+//     }
+// }
+
+// rememberNyFilms();
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         console.log("Просмотрено довольно мало фильмов");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log("Вы классический зритель");
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log("Вы киноман");
+//     } else {
+//         console.log("Произошла ошибка");
+//     }
+// }
+
+// detectPersonalLevel();
+
+// console.log(personalMovieDB);
